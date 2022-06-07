a0a5bf75c1c6b5fa1bae9091205fdfc99a00e9b0
Nick Nicolosi <nick.nicolosi@protonmail.com>
2022-06-07 13:23:18 -0400

    Switch to using port 5433 for postgres DB instead of default 5432 in order to avoid conflicts with existing postgres installations

--- server/src/config/secrets.ts
+++ server/src/config/secrets.ts
@@ -1,5 +1,5 @@
 export const dbHostname = 'localhost';
-export const dbPort = 5432;
+export const dbPort = 5433;
 export const dbName = 'technical-challenge';
 export const dbUsername = 'postgres';
 export const dbPassword = 'postgres';
